9aeb3884fb4149ef8c8793ffa7fd7d6a27cb57b8
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-01-30 18:59:45 -0800

    initial commit

---
+++ src/Main.purs
@@ -0,0 +1,22 @@
+module Main where
+
+import Prelude
+
+import Data.Array (all, filter)
+import Data.CodePoint.Unicode (isAsciiLower)
+import Data.String (joinWith, length, toUpper)
+import Data.String.CodePoints (toCodePointArray)
+import Data.String.Utils (lines)
+import Effect (Effect)
+import Node.Encoding (Encoding(ASCII))
+import Node.FS.Sync (readTextFile, writeTextFile)
+
+main :: Effect Unit
+main = do
+  str <- readTextFile ASCII "doc/words.txt"
+  writeTextFile ASCII "doc/words5.txt" $
+    joinWith "\n" $ map toUpper $ (filter only5CharLowerCase (lines str))
+
+only5CharLowerCase :: String -> Boolean
+only5CharLowerCase word =
+  (5 == length word) && (all isAsciiLower $ toCodePointArray word)
